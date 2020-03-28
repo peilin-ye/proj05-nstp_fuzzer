f11d337a01966be0e4f61e34f6e227edbe299a1c
Matías Sequeira <msequeira@MBP-de-Matias.fios-router.home>
2020-03-28 18:56:46 -0400

    Created program structure

---
+++ NSTPFuzzer.py
@@ -0,0 +1,246 @@
+import argparse
+import utils.ProtoCrafter
+import nacl.utils
+import utils.nstp_v3_pb2 as nstp_v3_pb2
+from utils.ProtoCrafter import craft_client_hello, craft_auth_request # TODO Add missing imports here 
+import nacl.bindings.crypto_kx as crypto_kx
+from nacl.bindings.crypto_secretbox import crypto_secretbox_open, crypto_secretbox, crypto_secretbox_NONCEBYTES 
+from nacl.bindings.randombytes import randombytes
+
+def serialize_send_and_receive(unserialized_proto_message, encrypt=True):
+    global server_address
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect(server_address)
+        print("Sent: {0}".format(unserialized_proto_message))
+        bytes_to_send= obj.SerializeToString()
+
+        if encrypt:
+            global client_tx
+            if client_tx=None:
+                print("Client TX  key not generated. CLosing.")
+                exit(1)
+
+            nonce= randombytes(crypto_secretbox_NONCEBYTES)
+            encrypted_bytes= crypto_secretbox(bytes_to_send, nonce, client_tx)
+            nstp_message= nstp_v3_pb2.NSTPMessage()
+            nstp_message.encrypted_message.ciphertext= encrypted_bytes
+            nstp_message.encrypted_message.nonce= nonce
+            bytes_to_send= nstp_message.SerializeToString()
+        
+        sock.sendall(len(bytes_to_send).to_bytes(2, byteorder="big") + bytes_to_send)
+
+        header = sock.recv(2)
+        message_length, = struct.unpack('>H', header) 
+        message= sock.recv(message_length)
+
+        nstp_message= nstp_v3_pb2.NSTPMessage()
+        nstp_message.ParseFromString(message)
+
+        # Here we shouldn't decrypt the message because we still don't know which type is it. We have to do it in each fuzz_...() function
+        return nstp_message
+
+def fuzz_client_hello(options):
+    if options.major:
+        print("[ClientHello] major={options.major}")
+    if options.minor:
+        print("[ClientHello] minor={options.minor}")
+    if options.user_agent:
+        print("[ClientHello] user_agent={options.user_agent}")
+    if options.public_key:
+        print("[ClientHello] public_key={options.public_key}"
+
+    for i in range(0,1000):
+        client_hello=craft_client_hello(options.major, options.minor, options.user_agent, options.public_key)
+
+        clien_hello_response= serialize_send_and_receive(client_hello, encrypt=False)    
+
+        # TODO check ServerHello/Error
+
+def fuzz_auth_request(options):
+    if options.username:
+        print("[AuthRequest] username={options.username}")
+    if options.password:
+        print("[AuthRequest] password={options.password}")    
+    
+    if options.public_key is None:
+        print("[AuthRequest] You must provide a public_key! Closing...")   
+        exit(1)
+
+    # TODO may be, this have to be in a loop? I mean, to send a client hello per AuthRequest? We'd have to adjust it as we test the servers
+    generate_session_keys(options.public_key)
+
+    for i in range(0,1000):
+        auth_request=craft_auth_request(options.username, options.password)
+
+        auth_request_response=serialize_send_and_receive(auth_request)
+
+        # TODO check AuthResponse/Error
+
+def fuzz_ping_request(options):
+    if options.data:
+        print("[PingRequest] username={options.data}")
+
+    if options.algo:
+        print("[PingRequest] algorithm={options.algo}")
+
+    if options.public_key is None:
+        print("[PingRequest] You must provide a public_key! Closing...")   
+        exit(1)
+
+    if options.password is None:
+        print("[PingRequest] You must provide a password! Closing...")   
+        exit(1)
+
+    if options.username is None:
+        print("[PingRequest] You must provide a username! Closing...")   
+        exit(1)
+
+    # First we have to generate the session keys and authenticate into the server
+    generate_session_keys(options.public_key)
+    auth_request=craft_auth_request(options.username, options.password)
+
+    # TODO verify auth request
+
+    for i in range(0,1000):
+        ping_request=craft_ping_request(options.data, options.algo)
+
+        ping_request_response=serialize_send_and_receive(ping_request)
+
+        # TODO check PingResponse/Error
+
+def fuzz_load_request(options):
+    # TODO. See PingRequest 
+    pass
+
+def fuzz_store_request(options):
+    # TODO. See PingRequest 
+    pass
+
+def generate_session_keys(public_key):
+    client_hello=craft_client_hello(1, 1, 'user_agent_test', public_key)
+    nstp_message_server_hello= serialize_send_and_receive(client_hello, encrypt=False)
+    server_public_key=nstp_message_server_hello.server_hello.public_key
+
+    # Generate the session keys
+    global client_public, client_private
+    global client_rx, client_tx
+    client_rx, client_tx = crypto_kx.crypto_kx_client_session_keys(client_public, client_private, server_public_key)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='NSTP Fuzzer')
+
+    requiredGroup = parser.add_argument_group('Required arguments')
+    requiredGroup.add_argument('--port',
+                                help='NSTP server port. eg. --port 22300',
+                                required = True,
+                                type=int)                
+    requiredGroup.add_argument('--ip',
+                                required = True,
+                                help='NSTP server IP. eg. --ip 192.168.1.2')
+    
+
+    # Options for the ClientHello.
+    parser.add_argument("--client-hello",
+                        help= 'Fuzz ClientHello messages. If none of the suboptions are provided, by default it will fuzz all the fields. On the other hand, if any suboption passed, it would be fixed to the value provided.',
+                        action= "store_true",
+                        default= None)
+    parser.add_argument("--major", 
+                        help='[ClientHello] Fixed major value to use. Otherwise, will be fuzzed.',
+                        type=int,
+                        default=None)       
+    parser.add_argument("--minor", 
+                        help='[ClientHello] Fixed minor value to use. Otherwise, will be fuzzed.',
+                        type=int,
+                        default=None)      
+    parser.add_argument("--agent", 
+                        help='[ClientHello] Fixed user agent to use. Otherwise, will be fuzzed.',
+                        default=None)               
+
+    # Options for the AuthenticationRequest.
+    parser.add_argument("--auth-request",
+                        help= 'Fuzz AuthenticationRequest messages. If none of the suboptions are provided, by default it will fuzz all the fields. On the other hand, if any suboption passed, it would be fixed to the value provided.',
+                        action= "store_true",
+                        default= None)
+    parser.add_argument("--user", 
+                        help='[AuthenticationRequest] Fixed user to use. Otherwise, will be fuzzed.',
+                        default=None) 
+    parser.add_argument("--pwd", 
+                        help='[AuthenticationRequest] Fixed password to use. Otherwise, will be fuzzed.',
+                        default=None)     
+
+    # Options for the PingRequest.
+    parser.add_argument("--ping-request",
+                    help= 'Fuzz PingRequest messages. If none of the suboptions are provided, by default it will fuzz all the fields. On the other hand, if any suboption passed, it would be fixed to the value provided.',
+                    action= "store_true",
+                    default= None)
+    parser.add_argument("--data", 
+                    help="""[PingRequest] Fixed data IN BYTES to use. Otherwise, will be fuzzed. e.g. "b'\x02\xe67\x00\xb8\'\xeb\xff'"  """,
+                    default=None) 
+    parser.add_argument("--algo", 
+                    help="[PingRequest] Fixed algorithm to use. Otherwise, will be fuzzed. 0-IDENTITY, 1-SHA-256 and 2-SHA-512",
+                    type=int,
+                    default=None)                  
+    
+    # Options for the LoadRequest.
+    parser.add_argument("--load-request",
+                    help= 'Fuzz LoadRequest messages. If none of the suboptions are provided, by default it will fuzz all the fields. On the other hand, if any suboption passed, it would be fixed to the value provided.',
+                    action= "store_true",
+                    default= None)
+    parser.add_argument("--load-key", 
+                        help='[LoadRequest] Fixed key to use. Otherwise, will be fuzzed.',
+                        default=None)    
+    parser.add_argument("--load-public-key", 
+                        help='[LoadRequest] Fixed boolean field to use. Otherwise, will be fuzzed. e.g. true/t or false/f',
+                        default=None)  
+
+    # Options for the StoreRequest.
+    parser.add_argument("--store-request",
+                    help= 'Fuzz StoreRequest messages. If none of the suboptions are provided, by default it will fuzz all the fields. On the other hand, if any suboption passed, it would be fixed to the value provided.',
+                    action= "store_true",
+                    default= None)
+    parser.add_argument("--store-key", 
+                        help='[StoreRequest] Fixed key to use. Otherwise, will be fuzzed.',
+                        default=None)
+    parser.add_argument("--store-value", 
+                        help='[StoreRequest] Fixed value to use. Otherwise, will be fuzzed.',
+                        default=None)    
+    parser.add_argument("--store-public-key", 
+                        help='[StoreRequest] Fixed boolean field to use. Otherwise, will be fuzzed. e.g. true/t or false/f',
+                        default=None)
+
+    # Parse required arguments for AuthenticationRequest, PingRequest, StoreRequest and LoadRequest
+    parser.add_argument("--public-key", 
+                        help='[ClientHello/AuthenticationRequest/PingRequest/StoreRequest/LoadRequest] Valid public key to exchange with server.',
+                        default=None) 
+    parser.add_argument("--username", 
+                        help='[AuthenticationRequest/PingRequest/StoreRequest/LoadRequest] Valid user to log into the server.',
+                        default=None)
+    parser.add_argument("--password", 
+                        help='[AuthenticationRequest/PingRequest/StoreRequest/LoadRequest] Valid password to log into the server.',
+                        default=None)          
+
+    options= parser.parse_args()
+
+    # IP and port are global so that send() function can obtain them easily
+    global server_address
+    server_address=(options.ip, options.port)
+
+    global client_public, client_private
+    client_public, client_private= crypto_kx.crypto_kx_keypair()
+
+    if options.client_hello:
+        fuzz_client_hello(options)
+    elif options.auth_request:
+        fuzz_auth_request(options)
+    elif options.ping_request:
+        fuzz_ping_request(options)
+    elif options.load_request:
+        fuzz_load_request(options)
+    elif options.store_request:
+        fuzz_store_request(options)
+    else:
+        print("No message selected. Closing.")
+        exit(0)
---
+++ utils/ProtoCrafter.py
@@ -0,0 +1,77 @@
+import nstp_v3_pb2, ast
+
+# If value provided remains None -> Just put a random value :) 
+
+def craft_client_hello(major=None, minor=None, user_agent=None, public_key=None):
+    
+    # TODO replace random() functions with real rand(), according to field type
+    if major is None:
+        major=random()
+
+    if minor is None:
+        minor=random()
+
+    if user_agent is None:
+        user_agent=random()
+
+    if public_key is None:
+        public_key=random()
+
+    # TODO Now that we have all the values, Serialize to protobuf!
+    proto_client_hello= nstp_v3_pb2.ClientHello()
+
+    return proto_client_hello
+
+def craft_auth_request(username=None, password=None):
+
+    # TODO replace random() functions with real rand(), according to field type
+    if username is None:
+        username=random()
+
+    if password is None:
+        password=random()
+
+    # TODO Now that we have all the values, Serialize to protobuf!
+    proto_auth_request= nstp_v3_pb2.AuthenticationRequest()
+
+    return proto_auth_request
+
+def craft_ping_request(data=None, algorithm=None):
+    # TODO replace random() functions with real rand(), according to field type
+    
+    if data is None:
+        data=random()
+    else:
+        # literal_eval takes the string "b'\x01....'" as bytes
+        data= ast.literal_eval(data)
+
+    if algorithm is None:
+        algorithm=random()
+    
+
+    # TODO Now that we have all the values, Serialize to protobuf!
+    proto_ping_request= nstp_v3_pb2.PingRequest()
+
+    return proto_ping_request
+
+def craft_store_request(key=None, value=None, is_public=None):
+    # TODO replace random() functions with real rand(), according to field type
+    if key is None:
+        key=random()
+
+    if value is None:
+        value=random()
+    else:
+        # literal_eval takes the string "b'\x01....'" as bytes
+        value=ast.literal_eval(value)
+
+    if is_public is None:
+        is_public=random()
+
+    # TODO Now that we have all the values, Serialize to protobuf!
+    proto_store_request= nstp_v3_pb2.StoreRequest()
+
+    return proto_store_request
+
+def craft_load_request(key=None, is_public=None):
+    # TODO
