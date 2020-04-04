b6c7cdedbb9ff68682a5f62daa136d78d3705ae4
Matías Sequeira <msequeira@MBP-de-Matias.fios-router.home>
2020-04-04 14:09:10 -0400

    If keys file not given,  generate and save them

--- NSTPFuzzer.py
+++ NSTPFuzzer.py
@@ -469,10 +469,20 @@ if __name__ == '__main__':
             logging.debug("client_public: {0}".format(client_public))
             logging.debug("client_private: {0}".format(client_private))
         except FileNotFoundError:
-            logging.error("\"./keys\" does not exist!")
-            exit(1)
+            logging.debug("\"./keys\" does not exist!")
+            
+            try:
+                client_public, client_private = crypto_kx.crypto_kx_keypair()
+                f = open("./keys", "wb")
+                f.write(client_public)
+                f.write(client_private)
+                logging.debug("Generated client key pair and stored in file \"./keys\".")
+                logging.debug("client_public: {0}".format(client_public))
+                logging.debug("client_private: {0}".format(client_private))
+                f.close()
+            except Exception as e:
+                logging.error("Error generating and saving keypair: {e}")
     else:
-        # randomly generate
         pass
        
 
